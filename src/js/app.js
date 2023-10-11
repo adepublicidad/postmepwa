385384fe7e67423d7152ab1e2f7de624c05e9efd
adepublicidad <angelwilson2078@hotmail.com>
2023-10-11 16:39:17 -0500

    cambio para instalar desde el boton

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,7 @@ let MAIN;
 let MODAL_POST;
 let BTN_SHOW_POST;
 let BTN_CANCEL_POST;
+let deferredPrompt
 
 // Funciones
 const showPostModal = () => {
@@ -16,6 +17,12 @@ const closePostModal = () => {
   MAIN.style.display = 'block';
   MODAL_POST.style.transform = 'translateY(100vh)';
 };
+
+// Codigo para que no carge el install
+window.addEventListener('beforeinstallprompt', (e) => {
+e.preventDefault();
+  deferredPrompt = e;
+});
 
 // Cuando se cargue todo nuestro DOM
 window.addEventListener('load',  () => {
@@ -37,3 +44,16 @@ if ('serviceWorker' in navigator) {
     }
   }
 }
+
+// Boton de Instalar
+
+const bannerInstall = document.querySelector('#banner-install');
+bannerInstall.addEventListener('click', () => {
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    const response =  deferredPrompt.userChoice;
+    if (response.outcome === 'dismissed') {
+      console.error('El usuario cancelo la instalación');
+    }
+  }
+});
